da6e1b934abd9f894f1449911f56dca8e9231d6e
shreyal0007 <shreyaljain0007@gmail.com>
2023-04-06 01:04:14 +0530

    myprofile page ui completed

--- src/pages/Myprofile/Myprofile.jsx
+++ src/pages/Myprofile/Myprofile.jsx
@@ -29,17 +29,100 @@ const Myprofile = () => {
           <div>
             <input
               type="text"
-              className="input"
+              className="input1"
               placeholder="tell something anout yourself"
             ></input>
           </div>
         </div>
         <div className="ciphermap">
-          <p className="aboutme">Cipher Map</p>
+          <p className="aboutme">CIPHER MAP</p>
           <img src={map} className="map"></img>
         </div>
-        <div className='ontheweb'>
-          
+        <div className="ontheweb">
+          <div className="title">
+            <p className="aboutme">ON THE WEB</p>
+            <button className="editbutton">Edit</button>
+          </div>
+          <div className="socials">
+            <div className="column">
+              <p className="socialsheading">Linkedin</p>
+              <input
+                type="link"
+                className="input"
+                placeholder="Linkedin"
+              ></input>
+              <p className="socialsheading">Github</p>
+              <input type="link" className="input" placeholder="Github"></input>
+              <p className="socialsheading">Facebook</p>
+              <input
+                type="link"
+                className="input"
+                placeholder="Facebook"
+              ></input>
+            </div>
+            <div className="column">
+              <p className="socialsheading">Twitter</p>
+              <input
+                type="link"
+                className="input"
+                placeholder="Twitter"
+              ></input>
+              <p className="socialsheading">Instagram</p>
+              <input
+                type="link"
+                className="input"
+                placeholder="Instagram"
+              ></input>
+              <p className="socialsheading">website</p>
+              <input
+                type="link"
+                className="input"
+                placeholder="Website"
+              ></input>
+            </div>
+          </div>
+        </div>
+
+        <div className="professionalinfo">
+          <div className="title">
+            <p className="aboutme">PROFESSIONAL INFORMATION</p>
+            <button className="editbutton">Edit</button>
+          </div>
+          <div className="column">
+            <p className="socialsheading">Highest Education</p>
+            <input
+              type="link"
+              className="input"
+              placeholder="graduation"
+            ></input>
+            <p className="socialsheading">What do you currently do? </p>
+            <input type="link" className="input" placeholder="College"></input>
+          </div>
+        </div>
+        <div className="security">
+          <div className="title">
+            <p className="aboutme">PASSWORD AND SECURITY</p>
+            <button className="editbutton">Edit</button>
+          </div>
+          <div className="column">
+            <p className="socialsheading">Password</p>
+            <input
+              type="password"
+              className="input2"
+              placeholder="enter your password"
+            ></input>
+          </div>
+        </div>
+        <div className="ontheweb">
+          <div className="title">
+            <p className="aboutme">INTERESTS</p>
+            <button className="editbutton">Edit</button>
+          </div>
+          <input
+            type="password"
+            className="input5"
+            placeholder="enter your interests"
+          ></input>
         </div>
       </div>
     </div>
