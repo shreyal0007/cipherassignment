27a4533d7ac64a2ad9a9f8966f5aa16ecafb43c8
shreyal0007 <shreyaljain0007@gmail.com>
2023-04-07 02:14:42 +0530

    UI for modals added for update profile, update password, interests (3 modals)

--- src/pages/Myprofile/Myprofile.jsx
+++ src/pages/Myprofile/Myprofile.jsx
@@ -1,15 +1,170 @@
-import React from 'react'
+import { useState } from "react"
+import React from "react"
+import {Modal} from "@mantine/core"
+import gitwall from "../../images/gitwall.png"
+import cross from "../../images/cross.png"
+
 import './Myprofile.css'
 import logo from "../../images/cipherlogo.jpeg"
 import map from "../../images/map.jpg"
 
 const Myprofile = () => {
+  const[modal,setModal]=useState(false);
+  const[modal1,setModal1]=useState(false);
+  const [modal2,setModal2]=useState(false);
+
   return (
     <div>
+      <Modal
+        opened={modal}
+        onClose={() => setModal(false)}
+        centered
+        overflow="inside"
+        overlayBlur="10"
+        withCloseButton={false}
+      >
+        <img
+          src={cross}
+          alt="close"
+          className="modalclosebutton"
+          onClick={() => setModal(false)}
+        />
+        <div className="modaltitle"> Profile Update</div>
+        <div className="modalbody">
+          <div className="modalcol1">
+            <img
+              src={logo}
+              alt="img not available"
+              className="modalimage"
+            ></img>
+          </div>
+          <div className="modalcol2">
+            <p className="heading1">First Name</p>
+            <input
+              type="text"
+              className="input8"
+              placeholder="First Name"
+            ></input>
+            <p className="heading1">Last Name</p>
+            <input
+              type="text"
+              className="input8"
+              placeholder="Last Name"
+            ></input>
+            <p className="heading1">Email</p>
+            <input type="text" className="input8" placeholder="Email"></input>
+            <p className="heading1">Phone Number</p>
+            <input
+              type="text"
+              className="input8"
+              placeholder="Phone Number"
+            ></input>
+          </div>
+        </div>
+        <div className="modalfooter">
+          <button
+            className="modalbuttonscancel"
+            onClick={() => setModal(false)}
+          >
+            Cancel
+          </button>
+          <button className="modalbuttonsave">Save</button>
+        </div>
+      </Modal>
+      <Modal
+        opened={modal1}
+        onClose={() => setModal1(false)}
+        centered
+        overflow="inside"
+        overlayBlur="10"
+        withCloseButton={false}
+      >
+        <img
+          src={cross}
+          alt="close"
+          className="modalclosebutton"
+          onClick={() => setModal1(false)}
+        />
+        <div className="modalbody">
+          <div className="modalcol2">
+            <p className="heading1">Current Password</p>
+            <input
+              type="password"
+              className="input8"
+              placeholder="Current Password"
+            ></input>
+            <p className="heading1">New Password</p>
+            <input
+              type="password"
+              className="input8"
+              placeholder="New Password"
+            ></input>
+            <p className="heading1">Confirm Password</p>
+            <input
+              type="password"
+              className="input8"
+              placeholder="Confirm Password"
+            ></input>
+          </div>
+        </div>
+        <div className="modalfooter">
+          <button
+            className="modalbuttonscancel"
+            onClick={() => setModal1(false)}
+          >
+            Cancel
+          </button>
+          <button className="modalbuttonsave">Save</button>
+        </div>
+      </Modal>
+      <Modal
+        opened={modal2}
+        onClose={() => setModal2(false)}
+        centered
+        overflow="inside"
+        overlayBlur="10"
+        withCloseButton={false}
+      >
+        <img
+          src={cross}
+          alt="close"
+          className="modalclosebutton"
+          onClick={() => setModal2(false)}
+        />
+
+        <div className="modalbody">
+          <div className="modal3col1">
+            <button className="orangebutton">App Development</button>
+            {/* <button className="bluebutton">Game Development</button> */}
+            <button className="bluebutton">Programming</button>
+            <button className="bluebutton">Data Science</button>
+          </div>
+          <div className="modal3col2">
+            <button className="orangebutton">Web Development</button>
+            <button className="orangebutton">Data Structures</button>
+            {/* <button className="orangebutton">Machine Learning</button> */}
+            <button className="bluebutton">Others</button>
+          </div>
+        </div>
+        <div className="modalfooter">
+          <button
+            className="modalbuttonscancel"
+            onClick={() => setModal(false)}
+          >
+            Cancel
+          </button>
+          <button className="modalbuttonsave">Save</button>
+        </div>
+      </Modal>
+
       <div className="header">
         <div className="col1">
           {" "}
-          <img src={logo} className="logoimage"></img>
+          <img
+            src={logo}
+            className="logoimage"
+            onClick={() => setModal(true)}
+          ></img>
         </div>
         <div className="col2">
           <p className="hello">Hello,</p>
@@ -36,7 +191,7 @@ const Myprofile = () => {
         </div>
         <div className="ciphermap">
           <p className="aboutme">CIPHER MAP</p>
-          <img src={map} className="map"></img>
+          <img src={gitwall} className="map"></img>
         </div>
         <div className="ontheweb">
           <div className="title">
@@ -102,7 +257,9 @@ const Myprofile = () => {
         <div className="security">
           <div className="title">
             <p className="aboutme">PASSWORD AND SECURITY</p>
-            <button className="editbutton">Edit</button>
+            <button className="editbutton" onClick={() => setModal1(true)}>
+              Edit
+            </button>
           </div>
           <div className="column">
             <p className="socialsheading">Password</p>
@@ -116,7 +273,7 @@ const Myprofile = () => {
         <div className="ontheweb">
           <div className="title">
             <p className="aboutme">INTERESTS</p>
-            <button className="editbutton">Edit</button>
+            <button className="editbutton" onClick={() => setModal2(true)}>Edit</button>
           </div>
           <input
             type="password"
